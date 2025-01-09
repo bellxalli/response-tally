1c625f4b37a736b3c438f083278467002471264b
X BELL <194058603+bellxalli@users.noreply.github.com>
2025-01-09 12:07:32 -0800

    Progress attempt at WAVE 2

--- src/Tallyer.java
+++ src/Tallyer.java
@@ -76,7 +76,7 @@ public class Tallyer {
      * Tally the occurrences of valid votes for each topic from the provided lists of IDs and topics.
      * 
      * The lists are of equal length and are aligned: the id at index zero cast a vote for
-     * the topic at endex 0, the id at index 1 cast a vote for the topic at index 1, etc.
+     * the topic at index 0, the id at index 1 cast a vote for the topic at index 1, etc.
      * It returns a map where each topic is associated with the number of times it appears in the input.
      * However, any user who did not enter exactly 2 topics should not have their votes counted.
      *
@@ -88,6 +88,47 @@ public class Tallyer {
       // WAVE 2
       // TODO: Implement this method
 
-      return null;
+      Map<String, Integer> topicCount = new HashMap<>();
+      Map<String, Integer> newIds = new HashMap<>();
+      
+      for(id : ids)
+      {
+        if(!newIds.containsKey(id))
+        {
+            newIds.put(id, 1);
+        }
+        else
+        {
+            int currentCount = topicCount.get(id);
+            int newCount = currentCount + 1;
+            topicCount.put(id, newCount);
+        }
+
+      }
+
+      for(id : newIds)
+      {
+        if(!newIds.get(id) < 3)
+        {
+            newIds.remove(id);
+        }
+      }
+      
+      for(String topic : topics)
+      {
+        
+        if (!topicCount.containsKey(topic)) 
+          {
+              topicCount.put(topic, 1);
+          }
+          else
+          {
+              int currentCount = topicCount.get(topic);
+              int newCount = currentCount + 1;
+              topicCount.put(topic, newCount);
+          }
+      }
+        
+        return null;
   }
 }
